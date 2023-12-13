73da2f6089da3d1dae168f0baeabbef14741fe3c
abelladius <szolosi.albert15@gmail.com>
2023-12-13 19:30:45 +0200

    Added github pages

--- src/components/testimonials/testimonials.component.jsx
+++ src/components/testimonials/testimonials.component.jsx
@@ -2,18 +2,6 @@ import React from 'react'
 import ProfilePicture from '../../assets/images/pp-2.png'
 import './testimonials.styles.scss'
 
-// What my clients say about me:
-//                 <p class="content">Discover the experiences and feedback of those who have journeyed with me. These testimonials are a testament to the collaborations, the impact, and the relationships forged through web development. </p>
-
-// <p class="content">-Alpinmag</p>
-//                 </div>
-//                 <p>"Damian is a highly skilled and dedicated web developer. He played a pivotal role in our e-commerce success. His expertise in OpenCart, plugin installations, banner designs, bug fixing, and SEO optimization significantly contributed to our online growth. We couldn't have achieved our goals without him."</p>
-
-// <p class="content">-KYD Animal Shelter</p>
-//                 </div>
-//                 <p>"Damian's commitment to our cause was truly exceptional. He designed and developed a website that allowed us to receive online donations effortlessly. His work was instrumental in furthering our mission to support stray animals. We're incredibly grateful for his contribution."</p>
-
-
 
 const Testimonials = () => {
   return (
