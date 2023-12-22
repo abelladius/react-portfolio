f8f6aa40537bebce73345d530ca8cb7f348f3dec
abelladius <szolosi.albert15@gmail.com>
2023-12-22 13:44:55 +0200

    Modified testimonial-card images

--- src/components/testimonials/testimonials.component.jsx
+++ src/components/testimonials/testimonials.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import ProfilePicture from '../../assets/images/pp-2.webp'
+import ProfilePictureSeb from '../../assets/images/testimonials/testimonials-seb.webp'
+import ProfilePictureJonathan from '../../assets/images/testimonials/testimonials-jonathan.webp'
 import './testimonials.styles.scss'
 
 
@@ -12,14 +13,14 @@ const Testimonials = () => {
 
         <div className='testimonial-cards-container'>
             <div className="testimonial-card">
-                <img src={ProfilePicture} alt="" />
+                <img src={ProfilePictureSeb} alt="" />
                 <h3 className="subtitle">Bela Sebastian</h3>
                 <h4>-AlpinMag</h4>
                 <p>"Damian is a highly skilled and dedicated web developer. He played a pivotal role in our e-commerce success. His expertise in OpenCart, plugin installations, banner designs, bug fixing, and SEO optimization significantly contributed to our online growth. We couldn't have achieved our goals without him."</p>
             </div>
 
             <div className="testimonial-card">
-                <img src={ProfilePicture} alt="" />
+                <img src={ProfilePictureJonathan} alt="" />
                 <h3 className="subtitle">Jhonatan K.</h3>
                 <h4>-KYDAnimalShelter</h4>
                 <p>"Damian's commitment to our cause was truly exceptional. He designed and developed a website that allowed us to receive online donations effortlessly. His work was instrumental in furthering our mission to support stray animals. We're incredibly grateful for his contribution."</p>
